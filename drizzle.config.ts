aae67ee04f8f3e4609a08931ebb1642bd331431c
Sky <skyblue3742@gmail.com>
2026-02-28 03:09:35 +0800

    fix: change drizzle driver to turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,8 @@ import type { Config } from 'drizzle-kit';
 const config: Config = {
     schema: './lib/schema.ts',
     out: './drizzle',
-    driver: 'libsql',
-    dbCredentials: {
-        url: process.env.TURSO_DATABASE_URL || 'file:sms.db'
-    }
+    driver: 'turso',
+    dbUrl: process.env.TURSO_DATABASE_URL || 'file:sms.db'
 };
 
 export default config;
