68ffd555a9db941e4dfd0f38b78c1dd965259005
Sky <skyblue3742@gmail.com>
2026-06-06 14:27:25 +0800

    fix: add limit to getAllSubscriptions

--- lib/services/pushSubscription.ts
+++ lib/services/pushSubscription.ts
@@ -2,6 +2,8 @@ import { db } from "@/lib/repo/db";
 import { pushSubscriptions } from "@/lib/repo/schema";
 import { eq } from "drizzle-orm";
 
+const MAX_SUBSCRIPTIONS = 100;
+
 export const deleteSubscription = async (endpoint: string) => {
   await db
     .delete(pushSubscriptions)
@@ -9,4 +11,4 @@ export const deleteSubscription = async (endpoint: string) => {
 };
 
 export const getAllSubscriptions = async () =>
-  db.select().from(pushSubscriptions);
+  db.select().from(pushSubscriptions).limit(MAX_SUBSCRIPTIONS);
